cf59d4000e97bbe4afcce2ed71a6925df6b2a08a
Brad Carter <brad@theworldsokayestprogrammer.com>
2025-01-26 11:46:37 -0800

    Refactored system metrics to SystemMetricsEmitter class, added random walk implementation

--- code-samples/prometheus-grafana/metrics-generator/metrics_generator.py
+++ code-samples/prometheus-grafana/metrics-generator/metrics_generator.py
@@ -1,17 +1,38 @@
-from prometheus_client import start_http_server, Gauge
+from prometheus_client import start_http_server, Counter, Gauge, Histogram, Summary
 import random
 import time
 
-# Create metrics
-cpu_usage = Gauge('cpu_usage_percent', 'CPU usage in percent')
-memory_usage = Gauge('memory_usage_bytes', 'Memory usage in bytes')
+def random_walk(min_val, max_val, max_step):
+    current = min_val + max_val / 2
+    while True:
+        current += random.randrange(-max_step, max_step)
+        if current < min_val:
+            current = min_val
+        elif current > max_val:
+            current = max_val
+        yield current
+
+class SystemMetricsEmitter:
+    cpu_mock = random_walk(0, 100, 10)
+    cpu_gauge = Gauge('cpu_usage_percent', 'CPU usage in percent')
+    memory_mock = random_walk(0, 16000000000, 1000000000)
+    memory_gauge = Gauge('memory_usage_bytes', 'Memory usage in bytes')
+    
+    def __init__(self):
+        pass
+
+    def emit(self):
+        self.cpu_gauge.set(next(self.cpu_mock))
+        self.memory_gauge.set(next(self.memory_mock))
 
 def generate_metrics():
+    systemMetrics = SystemMetricsEmitter()
     while True:
-        cpu_usage.set(random.uniform(0, 100))
-        memory_usage.set(random.uniform(0, 16000000000))
+        print("scrape")
+        systemMetrics.emit()
         time.sleep(5)
 
 if __name__ == '__main__':
+    print("Starting metrics generator")
     start_http_server(8000)
     generate_metrics()
