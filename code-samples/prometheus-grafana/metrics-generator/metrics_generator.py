fd155d0df35dcee90d064b340620ee97817d511f
Brad Carter <brad@theworldsokayestprogrammer.com>
2025-01-26 13:51:37 -0800

    added request metrics emitter

--- code-samples/prometheus-grafana/metrics-generator/metrics_generator.py
+++ code-samples/prometheus-grafana/metrics-generator/metrics_generator.py
@@ -12,6 +12,11 @@ def random_walk(min_val, max_val, max_step):
             current = max_val
         yield current
 
+def random_observations(min_observations, max_observations, min_observation_value, max_observation_value):
+    num_observations = random.randint(min_observations, max_observations)
+    observations = [random.randint(min_observation_value, max_observation_value) for _ in range(num_observations)]
+    return observations
+
 class SystemMetricsEmitter:
     cpu_mock = random_walk(0, 100, 10)
     cpu_gauge = Gauge('cpu_usage_percent', 'CPU usage in percent')
@@ -25,11 +30,26 @@ class SystemMetricsEmitter:
         self.cpu_gauge.set(next(self.cpu_mock))
         self.memory_gauge.set(next(self.memory_mock))
 
+class RequestMetricsEmitter:
+    request_counter = Counter('request_counter', 'Request counter')
+    request_latency_ms = Histogram('request_latency', 'Request latency', buckets=(5, 10, 50, 100, 500, 1000, 5000, 10000, 50000, 100000, 500000, 1000000, float("inf")))
+
+    def __init__(self):
+        pass
+
+    def emit(self):
+        observations = random_observations(50, 100, 1, 100000)
+        self.request_counter.inc(len(observations))
+        for observation in observations:
+            self.request_latency_ms.observe(observation)
+
 def generate_metrics():
     systemMetrics = SystemMetricsEmitter()
+    requestMetrics = RequestMetricsEmitter()
     while True:
         print("scrape")
         systemMetrics.emit()
+        requestMetrics.emit()
         time.sleep(5)
 
 if __name__ == '__main__':
