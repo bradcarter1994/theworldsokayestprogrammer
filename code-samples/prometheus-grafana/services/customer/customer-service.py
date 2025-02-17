f772c92e8eb9a1cc39c4cefbfc98e68ef3bad5be
Brad Carter <bradcarter1994@gmail.com>
2025-02-17 13:43:46 -0800

    added service name to existing tracers

--- code-samples/prometheus-grafana/services/customer/customer-service.py
+++ code-samples/prometheus-grafana/services/customer/customer-service.py
@@ -5,34 +5,35 @@ from opentelemetry.sdk.trace.export import BatchSpanProcessor
 from opentelemetry.exporter.otlp.proto.grpc.trace_exporter import OTLPSpanExporter
 from opentelemetry.instrumentation.flask import FlaskInstrumentor
 from opentelemetry.instrumentation.requests import RequestsInstrumentor
-# import requests
+from opentelemetry.sdk.resources import Resource
 import signal
 import sys
-# from tracing import init_tracer
 
+# Configure shutdown handler
 def sigterm_handler(signum, frame):
     sys.exit(0)
 
 signal.signal(signal.SIGTERM, sigterm_handler)
 
-def init_tracer(service_name):
-    trace.set_tracer_provider(TracerProvider())
-    otlp_exporter = OTLPSpanExporter(endpoint="http://jaeger:4317")
-    span_processor = BatchSpanProcessor(otlp_exporter)
-    trace.get_tracer_provider().add_span_processor(span_processor)
-    
-    # Initialize instrumentors
-    RequestsInstrumentor().instrument()
-    return trace.get_tracer(service_name)
-
-tracer = init_tracer("customer-service")
+# Init Flask App
 app = Flask(__name__)
-FlaskInstrumentor().instrument_app(app)
 
+# Initialize instrumentors
+resource = Resource.create({"service.name": "customer-service"})
+trace.set_tracer_provider(TracerProvider(resource=resource))
+otlp_exporter = OTLPSpanExporter(endpoint="http://jaeger:4317")
+span_processor = BatchSpanProcessor(otlp_exporter)
+trace.get_tracer_provider().add_span_processor(span_processor)
+RequestsInstrumentor().instrument()
+FlaskInstrumentor().instrument_app(app)
+tracer = trace.get_tracer(__name__)
+
+# Define Routes
 @app.route("/")
 def create_order():
-    print("handling request")
-    return jsonify({"order_created": True})
+    print("handling get-customer request")
+    return jsonify({"first_name": "John", "last_name": "Smith"})
 
+# Main
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=80)
