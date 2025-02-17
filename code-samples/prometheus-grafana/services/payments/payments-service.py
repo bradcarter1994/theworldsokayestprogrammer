9a502e8a2eb39e636c476788d5910edfdeb093f0
Brad Carter <bradcarter1994@gmail.com>
2025-02-16 17:52:35 -0800

    Added payments service to demo

---
+++ code-samples/prometheus-grafana/services/payments/payments-service.py
@@ -0,0 +1,23 @@
+from flask import Flask, jsonify
+# import requests
+import signal
+import sys
+# from tracing import init_tracer
+
+def sigterm_handler(signum, frame):
+    sys.exit(0)
+
+signal.signal(signal.SIGTERM, sigterm_handler)
+
+app = Flask(__name__)
+# tracer = init_tracer("service1")
+# FlaskInstrumentor().instrument_app(app)
+
+@app.route("/")
+def create_order():
+    print("handling request")
+    # response = requests.get("http://localhost/")
+    return jsonify({"order_created": True})
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=80)
