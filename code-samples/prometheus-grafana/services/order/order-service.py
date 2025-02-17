f6780c1344ec0dbec6fdb493bf80a87de31a22cf
Brad Carter <bradcarter1994@gmail.com>
2025-02-17 13:43:25 -0800

    added tracing to order service

--- code-samples/prometheus-grafana/services/order/order-service.py
+++ code-samples/prometheus-grafana/services/order/order-service.py
@@ -1,17 +1,33 @@
 from flask import Flask, jsonify
+from opentelemetry import trace
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.sdk.trace.export import BatchSpanProcessor
+from opentelemetry.exporter.otlp.proto.grpc.trace_exporter import OTLPSpanExporter
+from opentelemetry.instrumentation.flask import FlaskInstrumentor
+from opentelemetry.instrumentation.requests import RequestsInstrumentor
+from opentelemetry.sdk.resources import Resource
 import requests
 import signal
 import sys
-# from tracing import init_tracer
 
+# Configure shutdown handler
 def sigterm_handler(signum, frame):
     sys.exit(0)
 
 signal.signal(signal.SIGTERM, sigterm_handler)
 
+# Init Flask App
 app = Flask(__name__)
-# tracer = init_tracer("service1")
-# FlaskInstrumentor().instrument_app(app)
+
+# Initialize instrumentors
+resource = Resource.create({"service.name": "order-service"})
+trace.set_tracer_provider(TracerProvider(resource=resource))
+otlp_exporter = OTLPSpanExporter(endpoint="http://jaeger:4317")
+span_processor = BatchSpanProcessor(otlp_exporter)
+trace.get_tracer_provider().add_span_processor(span_processor)
+RequestsInstrumentor().instrument()
+FlaskInstrumentor().instrument_app(app)
+tracer = trace.get_tracer(__name__)
 
 @app.route("/")
 def create_order():
